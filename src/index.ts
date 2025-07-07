df0c14169085595fb7652fa533b6c0d1380e9b42
Ornella <ornella@Ornellas-MacBook-Pro.local>
2025-07-07 17:27:56 +0200

    added sequelize sync

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import commandeRoutes from './routes/commandeRoutes';
+import sequelize from './database/database';
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,13 @@ app.use('/commandes', commandeRoutes);
 app.get('/', (_req, res) => {
   res.send('Hello, Express + TypeScript 👋');
 });
+
+sequelize.sync().then(() => {
+  console.log('Base de données synchronisée');
+  app.listen(port, () => {
+    console.log(`API Produits prête sur http://localhost:${port}`);
+  });
+}).catch(console.error);
 
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://localhost:${port}`);
